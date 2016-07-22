55032880a10e6108589be978913c07dc244e4ca2
Jeff Eng <jeff@jeffeng.me>
2016-07-22 12:44:29 -0700

    added collectionView methods to conform to UICollectionViewDataSource and UICollectionViewDelegate protocols

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
@@ -21,6 +21,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
 
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
+        return cell
+    }
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # NameToFaces
-Repo following Project 10: Names to Faces with UICollectionView tutorial at www.hackingwithswift.com
+Repo following Project 10: Names to Faces with UICollectionView tutorial at www.hackingwithswift.com.  The goal of this project is to build an app that helps you remember names with the person's face.
+
+![App Screenshot](appscreenshot.png)
+
+## Concepts Learned/Practiced
+* UICollectionView
+* Creating a custom class
+
+## Attributions
+[Project 10: Name to Faces with UICollectionView](www.hackingwithswift.com/read/10/overview)
